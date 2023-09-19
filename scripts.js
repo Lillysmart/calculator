02f1d2e42b8775bc9b8084d82fa175e75a81131b
Lillysmart <137174363+Lillysmart@users.noreply.github.com>
2023-09-19 12:25:50 +0200

    get the inputs from the html to js

--- scripts.js
+++ scripts.js
@@ -1,1 +1,18 @@
-console.log ('hoi')
+const inputs= document.querySelectorAll('[data-key="number"]');
+const addButton =document.querySelector('[data-key="add"]')
+const result= document.querySelector('[data-key="result"]')  
+console.log(result)
+const equalTo = document.querySelector(' [data-key="equal_to"]')
+console.log(equalTo)
+const handleAdd =()=>{
+
+const add = inputs +inputs
+return result.innerHTML=add
+}
+addButton.addEventListener("click",console.log("okay"))
+
+inputs.forEach(input => {
+    input.addEventListener("click", () => {
+        console.log("hi");
+    });
+});
